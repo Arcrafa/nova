998a4a49a7c38edd16b44fb5dd6c14ee80e42b82
ArcRafa <maldonadoagudelorafael@gmail.com>
2022-05-09 20:37:39 -0500

    commit

--- myCVN/utils/architecture.py
+++ myCVN/utils/architecture.py
@@ -1,26 +1,7 @@
 import tensorflow as tf
 from tensorflow.keras.applications import EfficientNetB0
-from tensorflow.keras.layers.experimental import preprocessing
 from tensorflow.keras import layers
-
-"""
-def architecture(config):
-    # pixel maps stored as (16000,), reshape to each view
-    input = KL.Input(shape=(100,160), name='input')
-    joined = KL.Reshape((2,100,80,1), name='reshape_input')(input)
-    joined = KL.Lambda(lambda x: tf.unstack(x,axis=1), name='split')(joined)
-
-    # a modified version of mobilenet v2
-    output = mynet_graph(joined[0], joined[1])
-
-    # 1 output probability for WS
-    output = KL.Dense(config.num_classes, activation='softmax', name='out')(output)
-
-    model = Model(inputs=input, outputs=output)
-
-    return model
-
-"""
+from tensorflow.keras.layers.experimental import preprocessing
 
 
 def architecture(config):
@@ -36,7 +17,7 @@ def architecture(config):
     x = layers.BatchNormalization()(x)
 
     # top_dropout_rate = 0.2
-    x = layers.Dense(15, activation="relu", name="dense_pred0")(x)
+    #x = layers.Dense(15, activation="relu", name="dense_pred0")(x)
 
     x = layers.Dense(10, activation="relu", name="dense_pred1")(x)
 
